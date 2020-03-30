ae325bac9c52927f895b92c7d96db6a21a5d6387
PaulC61 <paulclarke61@gmail.com>
2020-03-30 21:09:17 +0200

    Relative Layout Converted to Constraint Layout

--- DataStorageManager/app/src/main/java/com/example/datastoragemanager/MainActivity.java
+++ DataStorageManager/app/src/main/java/com/example/datastoragemanager/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.datastoragemanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btnOk = findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(this);
     }
+    //if we use this, it will always jump to the below method
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.btnOk){
+            Log.i("btnOk", "btnOk");
+        }
+    }
+    //modify layout so not relLayout but consLayout
 }
