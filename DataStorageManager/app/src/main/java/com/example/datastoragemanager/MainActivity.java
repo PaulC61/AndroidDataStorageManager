d55d00f34e5fabe5491d39a11ec632d1021ee35e
PaulC61 <paulclarke61@gmail.com>
2020-03-31 11:24:26 +0200

    oh boi

--- DataStorageManager/app/src/main/java/com/example/datastoragemanager/MainActivity.java
+++ DataStorageManager/app/src/main/java/com/example/datastoragemanager/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.datastoragemanager;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public final static int DEFAULT = 0;
+    private final static String SHARED_PREFERENCES_FILENAME = "my-preferences-file";
+    private final static String UPDATE_INFO_KEY ="update-info";
+    private final static String EMPTY_STRING = "";
+
+    private EditText etInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +33,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(view.getId()==R.id.btnOk){
             Log.i("btnOk", "btnOk");
         }
+        storeUpdateInfoIntoPreferences();
+
     }
-    //modify layout so not relLayout but consLayout
+
+    private void storeUpdateInfoIntoPreferences() {
+        String info = etInfo.getText().toString();
+
+        if (updateInfoIsValid(info)) {
+            String simplifiedInfo = info.trim();
+            SharedPreferences sPrefs = getSharedPreferences(SHARED_PREFERENCES_FILENAME, MODE_PRIVATE);
+            SharedPreferences.Editor sPrefsEditor = sPrefs.edit();
+            sPrefsEditor.putString(UPDATE_INFO_KEY, simplifiedInfo);
+
+            etInfo.setText(EMPTY_STRING);
+            Toast.makeText(this, "String '" + simplifiedInfo + "' is saved", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Data not valid", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean updateInfoIsValid(String info){
+        return !info.trim().isEmpty();
+    }
+
+        //modify layout so not relLayout but consLayout
 }
